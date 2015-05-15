10196fee00875798d1a21ef0abdd0d80acccb5f2
SeventhLogic <cyrusskolnick@gmail.com>
2015-05-15 14:07:58 -0600

    1.) Got the places API to log without errors or leaks. (play services is out of date) 2.) Fixed crash issue with switch screen.
    PS still trying to understand how places works. It grabs the information but I don't understand how to display it.

--- app/src/main/java/com/example/jen/bar_locator/MainActivity.java
+++ app/src/main/java/com/example/jen/bar_locator/MainActivity.java
@@ -7,14 +7,44 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
 
-public class MainActivity extends Activity {
+
+
+public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{//These needed to be added for the below Connection callbacks and such
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();//Need some more research on all of this but basically as I understand this it is building the API and importing all of these settings
     }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
 
     public void goToHomePage (View view){
         Intent intent = new Intent(this, Manual_Search.class);
@@ -45,5 +75,20 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
