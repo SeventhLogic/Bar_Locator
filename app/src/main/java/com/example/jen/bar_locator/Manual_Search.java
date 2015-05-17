1760b435fe48740cb6035759b54edbe7c9a5ea67
BrockCarlson <brockwarwickcarlson@gmail.com>
2015-05-17 08:07:07 -0700

    Just made the button from manual_search go to the main activity page

--- app/src/main/java/com/example/jen/bar_locator/Manual_Search.java
+++ app/src/main/java/com/example/jen/bar_locator/Manual_Search.java
@@ -1,7 +1,9 @@
 package com.example.jen.bar_locator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Brock on 5/14/2015.
@@ -12,5 +14,12 @@ public class Manual_Search extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.manual_search);
+    }
+
+    public void submitManualSearch(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        this.isDestroyed();
+
     }
 }
