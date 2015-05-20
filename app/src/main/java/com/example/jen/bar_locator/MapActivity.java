c9de1283169f3f334152ea3e08cc50bdaf03ea0c
SeventhLogic <cyrusskolnick@gmail.com>
2015-05-20 17:12:24 -0600

    Got the marker on there now we have what we need to start linking database info to the marker method.

--- app/src/main/java/com/example/jen/bar_locator/MapActivity.java
+++ app/src/main/java/com/example/jen/bar_locator/MapActivity.java
@@ -14,11 +14,57 @@ import com.google.android.gms.maps.*;
  * Created by Cyrus on 5/20/2015.
  */
 public class MapActivity extends FragmentActivity
+    implements OnMapReadyCallback
 {
+
+    GoogleMap mMap;
+
     @Override
-    protected void onCreate(Bundle saveedInstanceState)
+    protected void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(saveedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.map_layout);
+        createMapView();
+        addMarker();
+
+
+        /*MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);*/
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+
+    private void createMapView()
+    {
+        try
+        {
+            if (null == mMap)
+            {
+                mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+            }
+
+            if (null == mMap)
+            {
+                Toast.makeText(getApplicationContext(),"Error Creating Map", Toast.LENGTH_SHORT).show();
+            }
+        }catch (NullPointerException e)
+        {
+            Log.e("MAPAPP", e.toString());
+        }
+    }
+
+    private void addMarker()
+    {
+        if(null != mMap)
+        {
+            mMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(0,0))
+                    .title("Marker"));
+        }
     }
 }
