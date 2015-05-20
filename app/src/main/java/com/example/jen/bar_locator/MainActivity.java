d4e00c808e8fccb0b8cc4e2ecbda3743d4eedeb5
SeventhLogic <cyrusskolnick@gmail.com>
2015-05-20 16:24:50 -0600

    Moved back to maps API. Map is up and running gents :D

--- app/src/main/java/com/example/jen/bar_locator/MainActivity.java
+++ app/src/main/java/com/example/jen/bar_locator/MainActivity.java
@@ -13,41 +13,18 @@ import com.google.android.gms.location.places.Places;
 
 
 
-public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{//These needed to be added for the below Connection callbacks and such
+public class MainActivity extends Activity {
 
-    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mGoogleApiClient = new GoogleApiClient
-                .Builder(this)
-                .addApi(Places.GEO_DATA_API)
-                .addApi(Places.PLACE_DETECTION_API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();//Need some more research on all of this but basically as I understand this it is building the API and importing all of these settings
-    }
-
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onStop()
-    {
-        mGoogleApiClient.disconnect();
-        super.onStop();
     }
 
 
     public void goToHomePage (View view){
-        Intent intent = new Intent(this, Manual_Search.class);
+        Intent intent = new Intent(this, MapActivity.class);
         startActivity(intent);
         this.isDestroyed();
 
@@ -75,20 +52,5 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-
     }
 }
