2541bfbbb98cf3948e9403aa6caef0292cda0a96
SeventhLogic <cyrusskolnick@gmail.com>
2015-05-24 14:01:12 -0600

    Got my location enabled and working on my phone. Removed play services error checking (this will be put back in later)

--- app/src/main/java/com/example/jen/bar_locator/MapActivity.java
+++ app/src/main/java/com/example/jen/bar_locator/MapActivity.java
@@ -8,7 +8,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.GooglePlayServicesUtil;//these will be used later just trying to figure out the error checking stuff
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -24,8 +24,8 @@ public class MapActivity extends FragmentActivity
     /*
     * Make it easier to use toasts
     * */
-    Context context = getApplicationContext();
-    int duration = Toast.LENGTH_SHORT;
+    //Context context = getApplicationContext();
+    //int duration = Toast.LENGTH_SHORT;
 
     private static final int REQUEST_RESOLVE_ERROR = 1001;
 
@@ -37,75 +37,10 @@ public class MapActivity extends FragmentActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_layout);
         createMapView();
+        mMap.setMyLocationEnabled(true);
         addMarker();
     }
-    /*
-    *  Called by Location Services when the request to connect the
-     * client finishes successfully. At this point, you can
-     * request the current location or start periodic updates
-    * */
 
-    //TODO figure out why the superclass isn't implemented on this file...
-    public void onConnected(Bundle dataBundle)
-    {
-        Toast.makeText(context, "Connected to location services", duration).show();
-    }
-
-    /*
-     * Called by Location Services if the connection to the
-     * location client drops because of an error.
-     */
-
-    public void onDisconnected()
-    {
-        Toast.makeText(context, "Disconnected from location services please reconnect", duration).show();
-    }
-
-    /*
-     * Called by Location Services if the attempt to
-     * Location Services fails.
-     */
-
-    public void onConnectionFailed(ConnectionResult connectionResult)
-    {
-        /*
-         * Google Play services can resolve some errors it detects.
-         * If the error has a resolution, try sending an Intent to
-         * start a Google Play services activity that can resolve
-         * error.
-         */
-        if(connectionResult.hasResolution())
-        {
-            try
-            {
-                //start an activity that tries to resolve the error
-                connectionResult.startResolutionForResult(
-                        this, REQUEST_RESOLVE_ERROR);
-                 /*
-                 * Thrown if Google Play services canceled the original
-                 * PendingIntent
-                 */
-            }catch (IntentSender.SendIntentException e)
-            {
-                //log the error
-                e.printStackTrace();
-            }
-        }
-        else
-        {
-            /*
-             * If no resolution is available, display a dialog to the
-             * user with the error.
-             */
-            Toast.makeText(context, "Connection Error has no resolution", duration).show();
-        }
-    }
-
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-
-    }
 
     private void createMapView()
     {
@@ -136,26 +71,9 @@ public class MapActivity extends FragmentActivity
         }
     }
 
-    protected boolean isPlayServicesAvailable()//a boolean to determine if Google Play Services is available
-    {
-        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
 
-        if(status == ConnectionResult.SUCCESS)
-        {
-            return true;
-        }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
 
-        else if(GooglePlayServicesUtil.isUserRecoverableError(status))
-        {
-            //deal with error
-        }
-        else
-        {
-            //maps is not available.
-        }
-
-        return (false);
     }
-
-
 }
