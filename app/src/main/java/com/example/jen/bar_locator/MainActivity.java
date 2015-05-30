a741df1e4eb41c1a2802c278bd65781594607ece
mhutch2b87 <mhutchinson2@yahoo.com>
2015-05-30 00:23:26 -0600

    Setup a differnt map page that will be compatible with another fragment page. Redid the layout as well as changed the android manafest so that on the enter button click it will run the displayMap_Activity() in main activity. I cannot for the life of me figure out why my Map_Activity.java and activity_map.xml are red. They will not run on the emulator. I plan on working on this tommorow but if you can take a look and see why my Map_Activity page and the layout are not working.....

--- app/src/main/java/com/example/jen/bar_locator/MainActivity.java
+++ app/src/main/java/com/example/jen/bar_locator/MainActivity.java
@@ -5,12 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
+import com.google.android.gms.maps.SupportMapFragment;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.places.Places;
-
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
 
 
 public class MainActivity extends Activity {
@@ -22,16 +23,17 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-
-    public void goToHomePage (View view){
-        Intent intent = new Intent(this, MapActivity.class);
+    public void displayMain_Activity(FragmentActivity fragmentActivity){
+        Intent intent = new Intent(this, Map_Activity.class);
         startActivity(intent);
         this.isDestroyed();
-
     }
+    //public void goToHomePage (View view){
+        //Intent intent = new Intent(this, MapActivity.class);
+       // startActivity(intent);
+       // this.isDestroyed();
 
-
-
+   // }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -46,7 +48,7 @@ public class MainActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+        //noinspecition SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
