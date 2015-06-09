0d95750668294f958fd50a34b3fa58aa4d191f99
BrockCarlson <brockwarwickcarlson@gmail.com>
2015-06-09 08:19:49 -0700

    IGNORE, DEBUG WILL NOT UPDATE. UPDATING GIT-HUB TO TRY AND RESOLVE THIS ISSUE TO ATTEMPT TO DEBUG.

---
+++ app/src/main/java/com/example/jen/bar_locator/Register.java
@@ -0,0 +1,23 @@
+package com.example.jen.bar_locator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Brock on 6/9/2015.
+ */
+public class Register extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_page);
+
+    }
+
+
+
+
+
+
+}
