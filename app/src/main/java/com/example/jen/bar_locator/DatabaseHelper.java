ffe105d35a2c6a721aaeaf06e70c213fa93ae638
SeventhLogic <cyrusskolnick@gmail.com>
2015-06-19 14:09:30 -0600

    got some database pull code inserted.

--- app/src/main/java/com/example/jen/bar_locator/DatabaseHelper.java
+++ app/src/main/java/com/example/jen/bar_locator/DatabaseHelper.java
@@ -2,8 +2,13 @@ package com.example.jen.bar_locator;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.nfc.Tag;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 /**
  * Created by Brock on 6/5/2015.
@@ -58,4 +63,31 @@ public class DatabaseHelper extends SQLiteOpenHelper{
 
     }
 
+    public boolean hasObject(String id)
+    {
+        SQLiteDatabase db = getWritableDatabase();
+        String selectString = "SELECT * FROM " + TABLE_NAME + "WHERE " + COL_1 + " =?";
+
+        Cursor cursor = db.rawQuery(selectString, new String[] {COL_1});
+
+        boolean hasObject = false;
+        if(cursor.moveToFirst())
+        {
+            hasObject = true;
+
+            //region if you had multiple records to check for, use this region.
+
+            int count = 0;
+            while (cursor.moveToNext())
+            {
+                count++;
+            }
+
+            //Log.d(Tag, String.format("%d records found", count));
+        }
+
+        cursor.close();
+        db.close();
+        return hasObject;
+    }
 }
